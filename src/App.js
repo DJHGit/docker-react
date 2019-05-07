2288c6046ddd795be799c1894fe096e8627f9062
DJHGit <djhgit@gmail.com>
2019-05-07 23:44:14 +0100

    app.js: helpful developer info to show on next AWS deployment

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Bye there, again and yet again!
+          Changed on the feature branch!
         </p>
       </div>
     );
